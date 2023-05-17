bce6042d4b30a19c8cddf1340a35b5f0256eb4ac
Bryan Hickey <bjh@bjh.dev>
2023-05-17 14:50:42 +1000

    nextjs13 with MUi and tailwind with `use client`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  important: '#__next',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
